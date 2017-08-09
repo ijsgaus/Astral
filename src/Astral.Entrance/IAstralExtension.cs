f367cfdc730d899bc7a1a5380c6482230285b76e
Ivan Simongauz <i.simongauz@corp.youdo.com>
2017-08-09 16:10:33 +0300

    1

--- src/Astral.Entrance/IAstralExtension.cs
+++ src/Astral.Entrance/IAstralExtension.cs
@@ -1,7 +1,15 @@
-﻿namespace Astral
+﻿using System;
+using Newtonsoft.Json.Linq;
+
+namespace Astral
 {
     public interface IAstralExtension
     {
-        
+        string Name { get; }
+    }
+
+    public interface IAstralSchemaExtension : IAstralExtension
+    {
+        Attribute ToAttribute(JProperty property);
     }
 }
---
+++ src/Astral.Entrance/ISchemaAttribute.cs
@@ -0,0 +1,14 @@
+﻿using Astral.Symbols;
+using Newtonsoft.Json.Linq;
+
+namespace Astral
+{
+    public interface ISchemaAttribute
+    {
+        string ExtensionName { get; }
+        JProperty ToProperty();
+        ISymbol ToSymbol();
+    }
+    
+    
+}
